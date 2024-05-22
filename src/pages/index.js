2bce0991e36c270ca38157614fce8278376d724a
Sushil Bhardwaj <46961049+sbrocks777@users.noreply.github.com>
2024-05-22 18:04:08 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  AboutSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="details" heading="Skills" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="github" heading="Contact" />
+      </Page>
+    </>
+  );
+}
